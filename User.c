0d5885b836a331e6965620f7294e5479711a4bc6
ehakobya <ehakobya@asu.edu>
2023-04-17 18:25:56 -0700

    Update

--- User.c
+++ User.c
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include "LoadBalancer.h"
+#include "HakobyanLoadBalancer.h"
 
 //forward declarations for internal (private) functions.
 void* simulate_user_request(void* user_id);
